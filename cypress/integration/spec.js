da94eb6578ff76fd05fe1dee2a6d08be860e611d
Chris Sargent <chris@stickypixel.com>
2020-10-22 21:06:58 +0200

    demonstration of mocked vs non-mocked cy.route

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,36 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("with mocked cy.route2 respone, there is no request body in cy.wait", () => {
+    const intercept = {
+      method: "POST",
+      pathname: "/graphql",
+    };
+    const response = {
+      body: {
+        data: {
+          hello: "world",
+        },
+      },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+
+    cy.route2(intercept, response).as("gql");
+    cy.visit(
+      "https://mapstaging.tacklehunger.org/donate?site=recOJoeAptDHrjhOC"
+    );
+    cy.wait("@gql").then((request) => console.log(request));
+  });
+  it("with NON-mocked cy.route2 respone, there is a request body in cy.wait", () => {
+    const intercept = {
+      method: "POST",
+      pathname: "/graphql",
+    };
+    cy.route2(intercept).as("gql");
+    cy.visit(
+      "https://mapstaging.tacklehunger.org/donate?site=recOJoeAptDHrjhOC"
+    );
+    cy.wait("@gql").then((request) => console.log(request));
+  });
+});
